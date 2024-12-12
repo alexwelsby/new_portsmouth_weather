d194816cc62dd29759f15ba3d7dab443137c3f56
the website devil <98803802+alexwelsby@users.noreply.github.com>
2024-12-12 14:02:17 +1300

    Added embed, weather icon handling, time_period handling
    Signed-off-by: the website devil <98803802+alexwelsby@users.noreply.github.com>

--- bot.py
+++ bot.py
@@ -15,6 +15,8 @@ from discord.ext import commands
 import redis
 import json
 
+import pytz
+
 load_dotenv()
 
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -37,6 +39,7 @@ start_time = 0
 uptime = 0
 
 bot_date = '2023-02-01'
+time_period = 'week'
 
 ALL_MODES = ["auto", "manual"]
 current_mode = ALL_MODES[0] #will be auto
@@ -58,10 +61,10 @@ async def on_ready():
 #parent group for weatherbot commands
 @bot.group(invoke_without_command=True)
 async def weather(ctx):
-    await ctx.send("Available subcommands: report, setdate, getdate. Use '!weather <subcommand>'.")
+    await ctx.send("Available subcommands: report <day|week>, set_date <date>, get_date. Use '!weather <subcommand> <optional:args>'.")
 
-def get_unix_date():
-    yyyymmdd = bot_date.split("-")
+def get_unix_date(date):
+    yyyymmdd = date.split("-")
     y = int(yyyymmdd[0])
     m = int(yyyymmdd[1])
     d = int(yyyymmdd[2])
@@ -70,42 +73,74 @@ def get_unix_date():
 
 
 @weather.command(name='report', help='Gets current weather for New Portsmouth.')
-async def report(ctx):
+async def report(ctx, args_time_period:str):
+    global time_period
+    time_period = args_time_period
     try:
-        result = get_current_json()
-        if result != "":
-            message = build_weatherman(result)
-            await ctx.send(message)
-        else:
-            await ctx.send(f"No event found for {bot_date}.")
+        async with ctx.typing():
+            result = get_current_json(time_period)
+            if result != "":
+                data, report = build_weatherman(result)
+                embed = create_embed(data, report, ctx)
+                await ctx.send(embed=embed)
+            else:
+                await ctx.send(f"No event found for {bot_date}.")
     except Exception as e:
         await ctx.send(f"Error blowing up data: {e}")
 
 def build_weatherman(result):
     averages = calculate_averages(result)
-    weather_type, weather_description = categorize_weather(result)
+    weather_type, weather_description, weather_icon = categorize_weather(result)
     season = categorize_season() #checks from global bot_date so needs nothing passed
     print(f"Season has been categorized as {season}.")
     temp_type = categorize_temperature(averages)
-
+    print(f"in build_weatherman, time_period is {time_period}")
     data = {
         'location': LOCATION,
-        'time_period': "day",
+        'time_period': time_period,
         'weather_description': weather_description, 
         'temp_min': averages['temp_min'],
         'temp_max': averages['temp_max'],
         'temp': averages['temp'],
+        'humidity': averages['humidity'],
         'precipitation': averages['precipitation'], 
         'season': season,
         'temp_type': temp_type,
         'weather': weather_type,
+        'weather_icon': weather_icon,
     }
 
     print(f"Final data structure is {data}.")
 
     weather_dat = weather_data(data)
     weatherman = weather_report()
-    return (weatherman.generate_report(weather_dat))
+    return data, weatherman.generate_report(weather_dat)
+
+def create_embed(data, weatherman_report, ctx):
+    weather_icon = data['weather_icon'][:-1] + get_day_or_night() #chops off the day/night indicator (we're going to match it to current day/night in the PNW)
+
+    embed = discord.Embed(title=f"This {data['time_period']}'s weather in {data['location']}",
+        color=ctx.guild.me.top_role.color,
+        timestamp=ctx.message.created_at,)
+    embed.set_author(name="New Portsmouth Weather", icon_url=f"https://openweathermap.org/img/wn/{weather_icon}.png")
+    embed.add_field(name="Description", value=f"**{weatherman_report}**", inline=False)
+    embed.add_field(name="Temperature(F)", value=f"**{data["temp"]}°F**", inline=True)
+    embed.add_field(name="Humidity(%)", value=f"**{data['humidity']}%**", inline=True)
+    embed.add_field(name="Average high (F)", value=f"**{data["temp_max"]}°F**", inline=True)
+    embed.add_field(name="Average low (F)", value=f"**{data["temp_min"]}°F**", inline=True)
+    embed.set_thumbnail(url=f"https://openweathermap.org/img/wn/{weather_icon}.png")
+    embed.set_footer(text=f"Requested by {ctx.author.name}")
+    return embed
+
+def get_day_or_night():
+    tz = pytz.timezone('US/Pacific')
+    stringified = str(datetime.now(tz)).split(' ')[1][:2] #will get us the hours of a current day in 24hr format
+    hours = int(stringified)
+    if hours > 18 or hours < 6:
+        return "n" #night is from 6pm to 6am (could do this by sunset/sunrise but not worth the bloat of more api checks)
+    else:
+        return "d"
+
 
 def build_info(data):
     pass
@@ -135,7 +170,8 @@ def categorize_weather(data):
     for entry in data:
         if entry['weather'][0]['main'] == top_weather:
            description = entry['weather'][0]['description']
-           return top_weather, description
+           icon = entry['weather'][0]['icon'] #gives us the png name for the openweatherapi
+           return top_weather, description, icon
         
     return top_weather, None
 
@@ -187,49 +223,47 @@ def calculate_averages(data):
 
 
 
-def get_current_json():
+def get_current_json(time_period):
     curMonth = bot_date[:-3] #chops it to yyyy-mm (what the json keys are)
     result = ""
     if redis_client.exists(curMonth):  #check if the key exists
         json_data = redis_client.execute_command('JSON.GET', curMonth)
         data = json.loads(json_data) #loads it into a list
-        result = [item for item in data if bot_date in item.get('dt_iso')] #fetches every hourly report on this date
+        if (time_period == 'day'):
+            result = [item for item in data if bot_date in item.get('dt_iso')] #fetches every hourly report on this date
+        elif (time_period == 'week'):
+            start_date = get_unix_date(bot_date)
+            end_date = start_date + 604800 #604800 is the # of seconds in a week
+            result = [item for item in data if item.get('dt') >= start_date and item.get('dt') <= end_date] #gets all reports between start and end unix times
+    print(result)
     return result
 
 
-@weather.command(name='setdate', help='Set the current date of the weather bot. (In auto mode, it will begin counting up from this date.)')
+@weather.command(name='set_date', help='Set the current date of the weather bot. (In auto mode, it will begin counting up from this date.)')
 async def setDate(ctx, *, date:str):
-    global bot_date
-    dt = parse(date)
-    bot_date = dt.strftime('%Y-%m-%d')
-    response = "My current date is " + bot_date + "."
-    await ctx.send(response)
+    async with ctx.typing():
+        global bot_date
+        dt = parse(date)
+        bot_date = dt.strftime('%Y-%m-%d')
+        response = "((OOC: My date has been set to " + bot_date + " (YYYY-MM-DD).))"
+        await ctx.send(response)
 
-@weather.command(name='getdate', help='Get the current date of the weather bot.')
+@weather.command(name='get_date', help='Get the current date of the weather bot.')
 async def getDate(ctx):
-    response = "My current date is " + bot_date + "."
-    await ctx.send(response)
+    async with ctx.typing():
+        response = "((OOC: My current date is " + bot_date + " (YYYY-MM-DD).))"
+        await ctx.send(response)
 
 @weather.command(name='uptime')
 async def getUptime(ctx):
-    days, hours, minutes, seconds = calculateUptime()
-    """if days % 7 == 0 & current_mode == "auto":
-        #if the bot is set to auto it advances the days itself, ergo:
-        advanceWeek()
-        change this so it happens automatically?!"""
-    formatted_uptime = f"Uptime: {days}d {hours}h {minutes}m {seconds}s"
-    await ctx.send(formatted_uptime)
-
-def createEmbed(json, city_name, ctx):
-    overview = json["overview"]
-    embed = discord.Embed(title=f"Weather in {city_name}",
-        color=ctx.guild.me.top_role.color,
-        timestamp=ctx.message.created_at,)
-    embed.add_field(name="Today's weather", value=f"**{overview}**", inline=False)
-    embed.set_thumbnail(url="https://i.ibb.co/CMrsxdX/weather.png")
-    embed.set_footer(text=f"Requested by {ctx.author.name}")
-    return embed
-
+        async with ctx.typing():
+            days, hours, minutes, seconds = calculateUptime()
+            """if days % 7 == 0 & current_mode == "auto":
+                #if the bot is set to auto it advances the days itself, ergo:
+                advanceWeek()
+                change this so it happens automatically?!"""
+            formatted_uptime = f"((OOC: Uptime: {days}d {hours}h {minutes}m {seconds}s.))"
+            await ctx.send(formatted_uptime)
 
 def calculateUptime():
     global uptime
@@ -243,7 +277,5 @@ def calculateUptime():
     seconds = seconds % 60
 
     return days, hours, minutes, seconds
-
-
 
 bot.run(TOKEN)
